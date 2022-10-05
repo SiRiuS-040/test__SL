97063b0a8e81cefcc201f854433dace71500af71
VladVoskresenskiy <vladislav.voskresenskiy@gmail.com>
2022-10-05 20:10:04 +0300

    сборка шрифтов

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,27 @@
 let projectFolder = 'build';
 let sourceFolder = 'src';
 
+let fs = require('fs');
+
 let path = {
     build: {
         html: projectFolder + '/',
         css: projectFolder + '/css/',
+        fonts: projectFolder + '/fonts/',
         js: projectFolder + '/js/',
         img: projectFolder + '/img/',
     },
     src: {
-        html: [sourceFolder + '/pug/*.pug'],
+        html: sourceFolder + '/pug/*.pug',
         css: sourceFolder + '/scss/style.scss',
+        fonts: sourceFolder + '/fonts/*.ttf',
         js: sourceFolder + '/js/script.js',
         img: sourceFolder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
     },
     watch: {
         html: sourceFolder + '/pug/**/*.pug',
         css: sourceFolder + '/scss/**/*.scss',
+        fonts: sourceFolder + '/fonts/*.ttf',
         js: sourceFolder + '/js/**/*.js',
         img: sourceFolder + '/img/**/*.{jpg,png,svg,gif,ico,webp}',
     },
@@ -35,7 +40,11 @@ let { src, dest } = require('gulp'),
     rename = require('gulp-rename'),
     uglify = require('gulp-uglify-es').default,
     imageMin = require('gulp-imagemin'),
-    pug = require('gulp-pug');
+    pug = require('gulp-pug'),
+    ttf2woff = require('gulp-ttf2woff'),
+    ttf2woff2 = require('gulp-ttf2woff2'),
+    fonter = require('gulp-fonter');
+
 
 
 function browserSync() {
@@ -60,7 +69,7 @@ function css() {
         .pipe(
             scss({
                 outputStyle: 'expanded'
-            })
+            }).on('error', scss.logError)
         )
         .pipe(groupMedia())
         .pipe(
@@ -89,6 +98,15 @@ function js() {
         .pipe(browsersync.stream())
 }
 
+function fonts() {
+    src(path.src.fonts)
+        .pipe(ttf2woff())
+        .pipe(dest(path.build.fonts))
+    return src(path.src.fonts)
+        .pipe(ttf2woff2())
+        .pipe(dest(path.build.fonts))
+}
+
 
 function images() {
     return src(path.src.img)
@@ -109,17 +127,90 @@ function watchFiles() {
     gulp.watch([path.watch.html], html);
     gulp.watch([path.watch.css], css);
     gulp.watch([path.watch.js], js);
-    gulp.watch([path.watch.img], images)
+    gulp.watch([path.watch.img], images);
 }
 
 function clean() {
     return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, images, css, html));
+gulp.task('otf2ttf', function () {
+
+    return gulp.src([sourceFolder + '/fonts/*.otf'])
+        .pipe(fonter({
+            formats: ['ttf']
+        }))
+        .pipe(dest(sourceFolder + '/fonts/'));
+})
+
+
+
+
+
+// const chalk = require('chalk');
+
+let srcFonts = 'src/scss/fonts.scss';
+let appFonts = 'build/fonts/';
+
+function fontsStyle(done) {
+    fs.writeFile(srcFonts, '', () => { });
+    fs.readdir(appFonts, (err, items) => {
+        if (items) {
+            let c_fontname;
+            for (let i = 0; i < items.length; i++) {
+                let fontname = items[i].split('.'),
+                    fontExt;
+                fontExt = fontname[1];
+                fontname = fontname[0];
+                if (c_fontname != fontname) {
+                    if (fontExt == 'woff' || fontExt == 'woff2') {
+                        fs.appendFile(srcFonts, `@include font-face("${fontname}", "${fontname}", 400);\r\n`, () => { });
+
+                    }
+                }
+                c_fontname = fontname;
+            }
+        }
+    })
+    done();
+}
+
+
+
+// function fontsStyle(params) {
+//     let file_content = fs.readFileSync(sourceFolder + '/scss/fonts.scss');
+//     if (file_content == '') {
+//         fs.writeFile(sourceFolder + '/scss/fonts.scss', '', cb);
+//         return fs.readdir(path.build.fonts, function (err, items) {
+
+//             console.log(items);
+//             if (items) {
+//                 let c_fontname;
+//                 for (var i = 0; i < items.length; i++) {
+//                     let fontname = items[i].split('.');
+//                     fontname = fontname[0];
+//                     if (c_fontname != fontname) {
+//                         fs.appendFile(sourceFolder + '/scss/fonts.scss', '@include font("' + fontname + '", "' + fontname + '", "400", "normal");\r\n', cb);
+//                     } c_fontname = fontname;
+//                 }
+//             }
+//         })
+//     }
+// }
+
+function cb() {
+
+}
+
+
+
+
+let build = gulp.series(clean, gulp.parallel(js, images, css, html, fonts), fontsStyle);
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
 
+exports.fontsStyle = fontsStyle;
+exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
 exports.css = css;
--- src/js/script.js
+++ src/js/script.js
@@ -1,126 +1,133 @@
 let fileArr = [];
+let emailTest = /^(|(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6})$/i;
+let nameTest = /^([А-Яа-яё -]{1,50}|[A-Za-z -]{1,50})$/;
 
 document.addEventListener('click', function (evt) {
-    //открыть меню
     if (evt.target && evt.target.closest('.navigation__open')) {
-        console.log('открыть меню');
-        let target = evt.target.closest('.navigation__open')
-        let navList = document.querySelector('.navigation__list')
-        target.classList.toggle('active')
+        let target = evt.target.closest('.navigation__open');
+        let navList = document.querySelector('.navigation__list');
+        target.classList.toggle('active');
         if (target.classList.contains('active')) {
-            navList.classList.add('active')
+            navList.classList.add('active');
         } else {
-            navList.classList.remove('active')
+            navList.classList.remove('active');
         }
     }
 
-
-
-    // клик по списку в меню
     if (evt.target && evt.target.closest('.navigation__link')) {
-        console.log('клик по списку в меню');
-        let navList = document.querySelector('.navigation__list')
-        let menuOpen = document.querySelector('.navigation__open')
-
-        navList.classList.remove('active')
-        menuOpen.classList.remove('active')
+        let navList = document.querySelector('.navigation__list');
+        let menuOpen = document.querySelector('.navigation__open');
+        navList.classList.remove('active');
+        menuOpen.classList.remove('active');
     }
 
-    // клик по выбору системы
     if (evt.target && evt.target.closest('.order-form__input--system')) {
-        console.log('клик по выбору системы');
-        let target = evt.target.closest('.order-form__input--system')
-
-        let parent = target.closest('.order-form__label--system')
-        parent.classList.toggle('select-active')
+        let target = evt.target.closest('.order-form__input--system');
+        let parent = target.closest('.order-form__label--system');
+        parent.classList.toggle('select-active');
     }
 
-    // закрытие выбора системы
     if (evt.target && !evt.target.closest('.order-form__label--system')) {
         let mainSelectLabel = document.querySelector('.order-form__label--system');
-        mainSelectLabel.classList.remove('select-active')
+        mainSelectLabel.classList.remove('select-active');
     }
 
-    // прикрепить файл
     if (evt.target && evt.target.closest('.order-form__input-title')) {
-        // console.log('прикрепить файл');
-        let target = evt.target.closest('.order-form__input-title')
-        let fileUpload = document.querySelector('.order-form__input--file')
+        let fileUpload = document.querySelector('.order-form__input--file');
         fileUpload.click();
     }
 
-    // удалить файл
     if (evt.target && evt.target.closest('.order-form__input-file-delete')) {
-        console.log('удалить файл');
-        evt.preventDefault()
-
-        let fileLabel = document.querySelector('.order-form__label--file')
-        let fileNameTitle = document.querySelector('.order-form__input-file-name')
-        let fileInput = document.querySelector('.order-form__input--file')
-
-        fileLabel.classList.remove('uploaded')
-        fileNameTitle.textContent = ''
-        fileInput.value = ''
+        evt.preventDefault();
+        let fileLabel = document.querySelector('.order-form__label--file');
+        let fileNameTitle = document.querySelector('.order-form__input-file-name');
+        let fileInput = document.querySelector('.order-form__input--file');
+        fileNameTitle.textContent = '';
+        fileInput.value = '';
         fileArr = [];
-
-
-
-        let target = evt.target.closest('.order-form__input-file-delete')
-
+        fileLabel.classList.remove('uploaded');
     }
-
 })
 
 function setFile(fileArr) {
     if (fileArr.length > 0) {
-        console.log('файл загружен');
-        let fileLabel = document.querySelector('.order-form__label--file')
-        let fileNameTitle = document.querySelector('.order-form__input-file-name')
-        fileLabel.classList.add('uploaded')
-        fileNameTitle.textContent = fileArr[0].name
+        let fileLabel = document.querySelector('.order-form__label--file');
+        let fileNameTitle = document.querySelector('.order-form__input-file-name');
+        fileLabel.classList.add('uploaded');
+        fileNameTitle.textContent = fileArr[0].name;
     } else {
-        fileLabel.classList.remove('uploaded')
+        fileLabel.classList.remove('uploaded');
     }
-
 }
 
 document.addEventListener('change', function (evt) {
-    //выбор системы
     if (evt.target && evt.target.classList.contains('system-select__radio-input')) {
-        console.log('выбор системы');
-        let target = evt.target
+        let target = evt.target;
         let mainSelect = document.querySelector('.order-form__input--system');
-        let parent = target.closest('.order-form__label--system')
-        mainSelect.value = target.value
-        parent.classList.remove('select-active')
+        let parent = target.closest('.order-form__label--system');
+        mainSelect.value = target.value;
+        parent.classList.remove('select-active');
     }
-
-
-    // загрузка файлов
     if (evt.target && evt.target.classList.contains('order-form__input--file')) {
-        console.log('загрузка файлов');
-        let target = evt.target
-
-
         fileArr = [];
         fileArr = Array.from(evt.target.files);
-
-        setFile(fileArr)
+        setFile(fileArr);
     }
-
-
-    // if (evt.target) {
-    //     console.log(evt.target);
-    //     // console.log('открыть меню');
-    // }
 })
 
+function sendform() {
+    if (document.forms[0].systemtype.value == "") {
+        document.forms[0].systemtype.focus();
+        return false
+    }
+    if (document.forms[0].email.value == "") {
+        document.forms[0].email.focus();
+        return false
+    }
+
+    if (document.forms[0].name.value == "") {
+        document.forms[0].name.focus();
+        return false
+    }
+    return true;
+}
+
+
 document.addEventListener('input', function (evt) {
-    // изменения range bar
     if (evt.target && evt.target.classList.contains('range-bar__input')) {
+        let target = evt.target;
+        let setValue = document.querySelector('.range-bar__value-data');
+        setValue.textContent = `${target.value} %`;
+    }
+    if (evt.target && evt.target.classList.contains('order-form__input--mail')) {
+        let target = evt.target;
+        let parentLabel = target.closest('.order-form__label');
+        if (target.value.length == 0) {
+            parentLabel.classList.remove('error');
+        } else if (target.value.length > 0) {
+            if (!(emailTest.test(target.value))) {
+                parentLabel.classList.add('error');
+            } else {
+                parentLabel.classList.remove('error');
+            }
+        } else {
+            parentLabel.classList.add('error');
+        }
+    }
+    if (evt.target && evt.target.classList.contains('order-form__input--name')) {
         let target = evt.target
-        let setValue = document.querySelector('.range-bar__value-data')
-        setValue.textContent = `${target.value} %`
+        let parentLabel = target.closest('.order-form__label');
+        if (target.value.length == 0) {
+            parentLabel.classList.remove('error');
+        } else if (target.value.length > 0) {
+            if (!(nameTest.test(target.value))) {
+                parentLabel.classList.add('error');
+            } else {
+                parentLabel.classList.remove('error');
+            }
+        } else {
+            parentLabel.classList.add('error');
+        }
     }
 })
 
